35cec3ca77cc53630c0ecfc8e35a425cf270ea3d
Thom Strub <thomstrub@gmail.com>
2022-08-24 14:43:05 -0700

    basic skeleton for elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { LayoutComponent } from './components/layout/layout.component';
 import { RecipeDetailComponent } from './components/recipeDetail/recipeDetail.component';
 import { RecipeItemComponent } from './components/recipeItem/recipeItem.component';
 import { RecipeListComponent } from './components/recipeList/recipeList.component';
-import { RootComponent } from './components/root/root.component';
 import { ShoppingListComponent } from './components/shoppingList/shoppingList.component';
+import { ShoppingListEditComponent } from './components/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RootComponent,
+    LayoutComponent,
     HeaderComponent,
     ShoppingListComponent,
     RecipeListComponent,
     RecipeItemComponent,
-    RecipeDetailComponent
+    RecipeDetailComponent,
+    ShoppingListEditComponent,
   ],
   imports: [
     BrowserModule
