e4251f62e0115abf579565eaaedc516558d5bff8
Thom Strub <thomstrub@gmail.com>
2022-08-27 11:24:47 -0700

    moves recipe code into separate component

--- src/app/components/recipes/recipeItem/recipeItem.component.ts
+++ src/app/components/recipes/recipeItem/recipeItem.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
     selector: 'app-recipeItem',
@@ -6,5 +6,5 @@ import { Component } from "@angular/core";
 })
 
 export class RecipeItemComponent {
-    
+    @Input() recipe: {name: string, description: string, imagePath: string};
 }
